4c98caa0e53138797ab2e59b3340cfb8d9dffb28
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-04-05 18:40:37 +0200

    :art: working on the Navbar adding dropdownmenu  :art:

---
+++ components/DropDownMenu.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+interface DropDownMenuProps{
+    onClose: () => void;
+}
+
+const DropDownMenu = ({onClose}:DropDownMenuProps) => {
+  return (
+    <div>DropDownMenu</div>
+  )
+}
+
+export default DropDownMenu
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,22 @@
+
+"use client"
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import logo from "@/public/assets/logo.svg";
+import { motion } from "framer-motion";
+import { AlignJustify, X } from "lucide-react";
+import DropDownMenu from "./DropDownMenu";
 
 const Navbar = () => {
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+  const toggleDropDown = () => {
+    setIsDropdownVisible(!isDropdownVisible);
+  };
+  const closeDropDown = () => {
+    setIsDropdownVisible(false);
+  };
+
   return (
     <div>
       <div className="p-6 md:p-7 flex items-center justify-between z-50">
@@ -20,12 +33,37 @@ const Navbar = () => {
           </Link>
         </div>
 
+
         <div className="cursor-pointer hidden md:flex space-x-10 items-center text-slate-300 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to bg-neutral-400 bg-opacity-50">
           <div className="hover:text-gray-50">Website Design</div>
-          <div className="hover:text-gray-50">Website Design</div>
-          <div className="hover:text-gray-50">Website Design</div>
-          <div className="hover:text-gray-50">Website Design</div>
+          <div className="hover:text-gray-50">Graphic Design</div>
+          <div className="hover:text-gray-50">Shopify Store</div>
+          <div className="hover:text-gray-50">Brands</div>
+
+          <Link href="/pricing" className="hover:text-gray-50">
+            Pricing
+          </Link>
         </div>
+
+
+        <div className="flex md:hidden">
+          {isDropdownVisible ? (
+            <div
+              onClick={toggleDropDown}
+              className="w-8 h-8 text-slate-300 cursor-pointer"
+            >
+              <X />
+              <DropDownMenu onClose={closeDropDown} />
+            </div>
+          ) : (
+            <AlignJustify
+              onAuxClick={toggleDropDown}
+              className="w-8 h-8 text-slate-300 cursor-pointer"
+            />
+          )}
+        </div>
+
+        
       </div>
     </div>
   );
