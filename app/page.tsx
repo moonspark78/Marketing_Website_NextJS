2451dce8e188075b505d26bb5ca8d50867848788
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-04-05 21:48:48 +0200

    :art: working on the Home  :art:

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,29 @@
 import Navbar from "@/components/Navbar";
 import { Spotlight } from "@/components/Ui/Spotlight";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="w-full h-screen md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <Navbar />
       <Spotlight
-        className="hidden md:flex md:leflt-80 md:-top-80"
+        className="md:flex leflt-80"
         fill="white"
       />
-      <div className="p-4 z-10 mx-auto relative w-full pt-10 md:pt-32 px-2">
+      <div className="p-4 z-10 mx-auto relative w-full pt-10 md:pt-20 px-2">
         <div className="text-4xl pb-5 md:text-7xl px-6 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to bg-neutral-400 bg-opacity-50">
-          
+          Create, grow and <br /> scale your business
         </div>
+        <p className="mt-4 text-lg font-normal text-neutral-300 max-w-lg text-center mx-auto px-4">
+          Custom tailored solutions for your business. We are a team of
+          creatives who are excited to help you grow your business.
+        </p>
+        <Link
+          href={"/book"}
+          className="cursor-pointer flex items-center justify-center border rounded-full w-48 p-2 mx-auto my-6 text-white"
+        >
+          Book a Call
+        </Link>
       </div>
     </div>
   );
